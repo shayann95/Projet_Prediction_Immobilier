a3f92898dcfc3aead0a30ad9f53cf62e0c62c8b1
MOHAMMAD Shayann <167557655+shayann95@users.noreply.github.com>
2026-02-16 23:34:44 +0100

    Refactor test_requests.py for clarity and formatting

--- serveur/test_requests.py
+++ serveur/test_requests.py
@@ -1,25 +1,24 @@
 import requests
 import json
-# URL de ton serveur Flask
+
 url = "http://127.0.0.1:5000/predict_home_price"
 
-# Données du formulaire (doivent correspondre EXACTEMENT à request.form dans Flask)
+
 payload = {
-    "location": '1st Phase JP Nagar',  # doit exister dans __locations
-    "total_sqft": 1000,                # nom exact attendu par Flask
+    "location": '1st Phase JP Nagar',  
+    "total_sqft": 1000,               
     "bhk": 3,
     "bath": 3
 }
 
-# Envoi de la requête POST comme formulaire
 response = requests.post(url, data=payload)
 
-# Affichage du code HTTP
+
 print("Status code:", response.status_code)
 
-# Essai de récupération du JSON renvoyé par Flask
+
 try:
     print("Response JSON:", response.json())
 except requests.exceptions.JSONDecodeError:
-    # Si Flask renvoie du texte brut ou une page d'erreur
+  
     print("Erreur JSON ! Contenu reçu :", response.text)
